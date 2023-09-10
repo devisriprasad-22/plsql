9156fe96986d044dc0e4db861323ab9b66c46e86
DEVISRIPRASAD <144002163+devisriprasad-22@users.noreply.github.com>
2023-09-10 20:44:36 +0530

    Add files via upload

---
+++ basic loop.sql
@@ -0,0 +1,13 @@
+DECLARE
+  v_counter NUMBER(2) := 1;
+BEGIN
+  LOOP
+    dbms_output.put_line('My counter is : '|| v_counter);
+    v_counter := v_counter + 1;
+    --IF v_counter = 10 THEN
+    --  dbms_output.put_line('Now I reached : '|| v_counter);
+    --  EXIT;
+    --END IF;
+    EXIT WHEN v_counter > 10;
+  END LOOP;
+END;
